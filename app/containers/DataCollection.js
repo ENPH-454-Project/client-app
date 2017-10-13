5fb4ef1be5d8c68f49d7dc3ec65936dbd9f17567
Colin MacLeod <colinlmacleod1@gmail.com>
2017-10-13 15:07:13 -0400

    Added spi code

--- app/containers/DataCollection.js
+++ app/containers/DataCollection.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import SPI from 'pi-spi'
+var SPI = require('pi-spi');
 
 
 var spi = SPI.initialize("/dev/spidev0.0")
 
 var data = [0,1,2,3,4]
 
+
+var spi = SPI.initialize("/dev/spidev0.0")
+
 export default class DataCollection extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      data:data
+      data:0
     }
     this.addData = this.addData.bind(this)
+    this.readData = this.readData.bind(this)
   }
 
   addData(array) {
@@ -22,12 +27,19 @@ export default class DataCollection extends React.Component{
       data:newArray
     })
   }
+  readData() {
+    spi.read(10, (data)=>{
+      this.setState({
+        data:data
+      })
+    })
+  }
   render() {
     return (
       <div>
         <p>Data collection</p>
         <p>{this.state.data}</p>
-        <button onClick={()=>this.addData([1,2,3,4])}>Add</button>
+        <button onClick={()=>this.readData()}>Add</button>
       </div>
     )
   }
