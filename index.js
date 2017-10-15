02892a060d4b7ade03a88a519a496a0f8377094b
Colin MacLeod <colinlmacleod1@gmail.com>
2017-10-15 18:12:25 -0400

    added a server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+
+//Middleware
+app.use(cors())
+app.use(bodyParser.json())
+
+axios.post('https://vdlmikqfqd.execute-api.us-east-1.amazonaws.com/prod/dsp-service', {
+  data:[1,2,3,4,5]
+  })
+  .then(function(res) {
+    console.log(res.data)
+  })
+  .catch(function(error) {
+    console.log(error.response.status + ', ' + error.response.statusText)
+  })
+
+  // Server Port
+app.listen(3000,function() {
+	console.log('App listening on port', port)
+})
+ module.exports = app;
