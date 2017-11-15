7cdd5c3acaaac7ef9fe8f4bc670ca57b0a07c650
Colin MacLeod <colinlmacleod1@gmail.com>
2017-11-15 10:04:24 -0500

    sdfg

--- Python/plot.py
+++ Python/plot.py
@@ -1,13 +1,14 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 100, 0, 100])
+plt.axis([0, 100, -50, 50])
 plt.ion()
 
-for i in range(10000):
+for i in range(100):
     if i>100:
-        plt.axis([i-100, i, 0, 100])
-    y = np.random.random()*100
+        plt.axis([i-100, i, -50, 50])
+    y =  50*np.sin(i*0.1)*(np.random.random()*0.4+0.6)
     plt.scatter(i, y)
     plt.pause(0.005)
 
