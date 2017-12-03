dbb01d428438079340f1ec7171be9c8b352faf53
Colin MacLeod <colinlmacleod1@gmail.com>
2017-12-03 09:51:14 -0500

    sadf

--- Python/testApp.py
+++ Python/testApp.py
@@ -1,37 +1,82 @@
 from Tkinter import Tk, Label, Button
 import numpy as np
 import random
+import wave
+from scipy.io.wavfile import read, write
+import pyaudio
+
 
 on = False
 i=0
 data = np.array([])
 sound = ''
+audio = np.array([])
+
+def load():
+    global audio
+    audio = wave.open()
 
 def start():
     print 'start'
     global on
     on = True
 
+def DSP(data):
+
 def stop():
     global on, sound, data
     on = False
     #break
     np.savetxt('data.csv',data, delimiter=',')
+    DSP(data)
 
 
 def play():
     global sound
-    print sound
-    #stream.write(sound)
+    filepath = 'rec.wav'
+    chunk = 1024
+    #open a wav format music
+    signal = wave.open(filepath)
+    print(signal)
+    #instantiate PyAudio
+    _pyaudio = pyaudio.PyAudio()
+    #open stream
+    stream = _pyaudio.open(format=_pyaudio.get_format_from_width(signal.getsampwidth()),
+                           channels=signal.getnchannels(),
+                           rate=signal.getframerate(),
+                           output=True)
+    #read data
+    data = signal.readframes(chunk)
+
+    #play stream
+    while data:
+        stream.write(data)
+        data = signal.readframes(chunk)
+
+    #stop stream
+    stream.stop_stream()
+    stream.close()
+
+    #close PyAudio
+    _pyaudio.terminate()
+
+def loadAll():
+
+
+
+
+
+def saveAs():
 
 
 
 class App:
     def __init__(self, master):
+        global on
         self.master = master
         master.title("A simple GUI")
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text=on)
         self.label.pack()
 
         self.start_button = Button(master, text="Start", command=start)
@@ -43,8 +88,15 @@ class App:
         self.play_button = Button(master, text="Play", command=play)
         self.play_button.pack()
 
+        self.load_button = Button(master, text="Load", command=loadAll)
+        self.load_button.pack()
+
+        self.save_button = Button(master, text="Save", command=saveAs)
+        self.save_button.pack()
+
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
+
 
     def greet(self):
         print("Greetings!")
@@ -53,12 +105,10 @@ def read():
     global on, data, sound,i
     if on:
         value = random.randint(0,9)
-        #print value
+        print chr(value)
         data = np.append(data,value)
-        # hang out and do nothing for a half second
-        sound += chr(int(data[i]))
-        print sound
-        i += 1
+        sound += chr(value)
+        #print data[i]
 
     root.after(100,read)
     #root.after(100, read())
